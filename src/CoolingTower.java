527c3ee3f9949b1b99d97463bf201ec398681e49
Jakub Dekař <Jakub.Dekar.s01@osu.cz>
2025-10-23 10:21:34 +0200

    Add nuclear power plant system implementation

---
+++ src/CoolingTower.java
@@ -0,0 +1,17 @@
+public class CoolingTower {
+    private double height;
+    private double diameter;
+    
+    public CoolingTower(double height, double diameter) {
+        this.height = height;
+        this.diameter = diameter;
+    }
+    
+    public double getHeight() {
+        return height;
+    }
+    
+    public double getDiameter() {
+        return diameter;
+    }
+}
